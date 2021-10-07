b3dd7684f7c859053367653dc74eb2f8481942e0
jMisch44 <jennimisch@gmail.com>
2021-10-07 19:28:22 -0400

    responds with almost complete recipe, need to add ingredients as an array

--- api/model.js
+++ api/model.js
@@ -1,20 +1,36 @@
 const db = require('../data/db-config')
 
-function getRecipeById(recipe_id) {
-    const data = db('recipes as r')
+async function getRecipeById(recipe_id) {
+    const recipeObjsArray = await db('recipes as r')
     .leftJoin('steps as s', 's.recipe_id', 'r.recipe_id')
+    .join('steps_ingredients as si', 's.step_id', 'si.step_id')
+    .join('ingredients as i', 'si.ingredient_id', 'i.ingredient_id')
     .select(
         'r.recipe_id',
         'r.recipe_name',
         'r.created_at',
         's.step_id',
         's.step_number',
-        's.step_instruction'
+        's.step_instruction',
+        'si.quantity',
+        'i.ingredient_id',
+        'i.ingredient_name'
         )
-    .where('recipe_id', recipe_id)
-    .orderBy('s.step_number')
-
-    return data;
+    .where('r.recipe_id', recipe_id)
+    const recipe = {
+        recipe_id : recipeObjsArray[0].recipe_id,
+        recipe_name: recipeObjsArray[0].recipe_name,
+        created_at: recipeObjsArray[0].created_at,
+        steps: recipeObjsArray.map(obj => {
+            return {
+                step_id: obj.step_id,
+                step_number: obj.step_number,
+                step_instructions: obj.step_instructions,
+                ingredients: [],
+            }
+        })
+      }
+      return recipe;
 }
 
 module.exports = {
